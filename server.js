acee5dcf920e6bd8bc5b9abe56727599bd537668
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2024-09-09 15:23:44 +0530

    railway integrated

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("/", async (req, res) => {
       <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Geeksynergy Backend</title>
+          <title>WISDOM-PEAK-ANALYTICS Backend</title>
           <style>
               body {
                   display: flex;
